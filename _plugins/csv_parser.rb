36e607fea83d00540b43a7d0512cbe2080a526aa
Akos <akos@wiresuncrossed.co.nz>
2024-12-11 00:35:53 +0100

    Add json populator

--- _plugins/csv_parser.rb
+++ _plugins/csv_parser.rb
@@ -1,15 +1,15 @@
-require 'csv'
+# require 'csv'
 
-module Jekyll
-  module CSVParser
-    def parse_csv(input)
-      result = []
-      CSV.parse(input, col_sep: ';', headers: true) do |row|
-        result << row.to_h
-      end
-      result
-    end
-  end
-end
+# module Jekyll
+#   module CSVParser
+#     def parse_csv(input)
+#       result = []
+#       CSV.parse(input, col_sep: ';', headers: true) do |row|
+#         result << row.to_h
+#       end
+#       result
+#     end
+#   end
+# end
 
-Liquid::Template.register_filter(Jekyll::CSVParser)
+# Liquid::Template.register_filter(Jekyll::CSVParser)
